e6a32ee4747745f26739a6f3e8de9c3871c49543
Aulkhami <rakha.tblt@gmail.com>
2025-10-24 19:35:15 +0700

    Add base concept of menu nav button

---
+++ src/lib/components/MenuNavButton.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import { page } from '$app/state';
+	import { Button } from './ui/button';
+
+	let { href, children } = $props();
+	let selected = $derived(() => {
+		return page.url.pathname === href;
+	});
+</script>
+
+<Button {href} variant="ghost" class={selected() ? 'border-b-2 border-blue-400' : ''}>
+	{@render children?.()}
+</Button>
--- src/routes/home/+layout.svelte
+++ src/routes/home/+layout.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import MenuNavButton from '$lib/components/MenuNavButton.svelte';
 	import type { LayoutProps } from './$types';
 
 	let { data, children }: LayoutProps = $props();
@@ -7,8 +8,9 @@
 <div class="px-16">
 	<header class="sticky top-0 w-full bg-transparent">
 		<div class="flex justify-around py-4">
-			<div class="flex grow justify-start">
+			<div class="flex grow justify-start gap-8">
 				<div class="text-2xl">CodeOut</div>
+				<MenuNavButton href="/home">Home</MenuNavButton>
 			</div>
 		</div>
 	</header>
