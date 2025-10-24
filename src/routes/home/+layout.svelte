ce7aca70c3006fc216e2058ecacb41a3c14a7a70
Aulkhami <rakha.tblt@gmail.com>
2025-10-24 12:13:58 +0700

    Create base home route

---
+++ src/routes/home/+layout.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import type { LayoutProps } from './$types';
+
+	let { data, children }: LayoutProps = $props();
+</script>
+
+<div class="px-16">
+	<header class="sticky top-0 w-full bg-transparent">
+		<div class="flex justify-around py-4">
+			<div class="flex grow justify-start">
+				<div class="text-2xl">CodeOut</div>
+			</div>
+		</div>
+	</header>
+
+	{@render children()}
+</div>
