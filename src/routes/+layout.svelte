e4a59e847f04cc2bdd5bb661e82aba51bc15de40
Aulkhami <rakha.tblt@gmail.com>
2025-10-24 09:45:20 +0700

    Add dark mode and set it as default

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,10 @@
 <script lang="ts">
 	import '../app.css';
+	import { ModeWatcher } from 'mode-watcher';
 	import favicon from '$lib/assets/favicon.svg';
+
+	import { setMode } from 'mode-watcher';
+	setMode('dark');
 
 	let { children } = $props();
 </script>
@@ -9,4 +13,5 @@
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
+<ModeWatcher />
 {@render children?.()}
