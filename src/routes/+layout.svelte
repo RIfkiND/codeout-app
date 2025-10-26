08b6554e8418a7f9ea6dae2aa905a85ec9439950
Aulkhami <rakha.tblt@gmail.com>
2025-10-26 21:00:30 +0700

    Fix unresolved merge conflict

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,13 +6,13 @@
 	import { onMount } from 'svelte';
 	import type { LayoutData } from './$types';
 	import { createSupabaseLoadClient } from '$lib/supabaseClient';
+	import { setMode } from 'mode-watcher';
 
-<<<<<<< HEAD
 	let { children, data }: { children: any; data: LayoutData } = $props();
 
 	onMount(() => {
 		const supabase = createSupabaseLoadClient();
-		
+
 		const { data: authListener } = supabase.auth.onAuthStateChange((event, session) => {
 			if (event === 'SIGNED_IN' || event === 'SIGNED_OUT') {
 				invalidate('supabase:auth');
@@ -21,12 +21,8 @@
 
 		return () => authListener.subscription.unsubscribe();
 	});
-=======
-	import { setMode } from 'mode-watcher';
-	setMode('dark');
 
-	let { children } = $props();
->>>>>>> 9e4402bebfde05d2208c795560dd705838f391c0
+	setMode('dark');
 </script>
 
 <svelte:head>
